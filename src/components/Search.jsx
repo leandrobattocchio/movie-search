f3c4ff3707a7e972901c2a6c9f708aa4c9ea5239
Leandro Battocchio <leandrobattocchio@hotmail.com>
2023-02-24 17:42:14 -0300

    movie search

---
+++ src/components/Search.jsx
@@ -0,0 +1,43 @@
+import { useId } from "react";
+
+const Search = ({
+  search,
+  updateSearch,
+  handleSubmit,
+  error,
+  debounceGetMovies,
+  handleSort,
+  sort,
+}) => {
+  const orderId = useId();
+
+  return (
+    <header>
+      <h1>Movie search</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          value={search}
+          onChange={({ target }) => {
+            updateSearch(target.value);
+            debounceGetMovies({ search: target.value });
+          }}
+          type="text"
+          placeholder="Spiderman, Batman, Scarie Movie..."
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+        <input
+          id={orderId}
+          type="checkbox"
+          onChange={handleSort}
+          checked={sort}
+        />
+        <label htmlFor={orderId}>Order with title</label>
+      </form>
+      {error ? <h5 className="error">{error}</h5> : null}
+    </header>
+  );
+};
+
+export default Search;
---
+++ src/constants/movie.js
@@ -0,0 +1,6 @@
+export const movieApi = {
+    KEY: '4287ad07',
+    URL: function () {
+        return `https://www.omdbapi.com/?apikey=${this.KEY}`
+    }
+}
